4d1eb24199f8408b394e3b3171dae267dc6c4d21
agonzalesbm <andres.gonzales.dev@gmail.com>
2024-06-18 22:21:50 -0400

    [@agonzalesbm] test folder example file added and nugget to work with that added

---
+++ backend-service/Test/TestExample.cs
@@ -0,0 +1,12 @@
+﻿using Xunit;
+
+namespace final_project.test {
+    public class TestExample 
+    {
+        [Fact]
+        public void Example_Test()
+        {
+            Assert.True(true);
+        }
+    }
+}
