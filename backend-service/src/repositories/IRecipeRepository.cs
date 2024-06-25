eb6a6a523449bac56a7e12214debddd5981c9559
Maria Rene Huanca Aliaga <huancaaliagamariarene@gmail.com>
2024-06-24 23:59:25 -0400

    delete comment

--- backend-service/src/repositories/IRecipeRepository.cs
+++ backend-service/src/repositories/IRecipeRepository.cs
@@ -7,7 +7,7 @@ namespace backend_service.src.repositories
   public interface IRecipeRepository
   {
     Task<IEnumerable<Recipe>> GetAllRecipes();
-    Task<Recipe> GetRecipeById(string id); // Cambiado a GetRecipeById
+    Task<Recipe> GetRecipeById(string id);
     Task AddRecipe(Recipe recipe);
     Task UpdateRecipe(string id, Recipe recipe);
     Task DeleteRecipe(string id);
